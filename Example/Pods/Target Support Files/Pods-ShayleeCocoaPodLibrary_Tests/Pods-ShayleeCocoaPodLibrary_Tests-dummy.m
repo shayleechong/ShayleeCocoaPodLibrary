6e9706cd9f95e0c4b06c5d7be674415116d0285d
Hui Yee <huiyee.chong@techstudio.com.sg>
2018-06-27 15:58:49 +0800

    Initial commit with pod lib

---
+++ Example/Pods/Target Support Files/Pods-ShayleeCocoaPodLibrary_Tests/Pods-ShayleeCocoaPodLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShayleeCocoaPodLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ShayleeCocoaPodLibrary_Tests
+@end
---
+++ Example/Pods/Target Support Files/ShayleeCocoaPodLibrary/ShayleeCocoaPodLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ShayleeCocoaPodLibrary : NSObject
+@end
+@implementation PodsDummy_ShayleeCocoaPodLibrary
+@end
